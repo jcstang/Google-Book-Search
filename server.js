91c725dd87cb0d2aad23117ac5aa54b6ea128bf0
Brian Allen <brianallen2401@gmail.com>
2020-05-08 11:34:02 -0600

    added axios package. updated mongoose connection in server.

--- server.js
+++ server.js
@@ -4,20 +4,27 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose.connection("mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+});
+
+
+// Send every request to the React app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connection("mongodb://localhost/googlebooks", { useNewUrlParser: true });
-// Send every request to the React app
-
+//Define API routes here
 
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
