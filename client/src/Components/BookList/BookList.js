3ac6884f21eb0bc880eef96c5a79e760fc7a1e30
Brian Allen <brianallen2401@gmail.com>
2020-05-11 10:56:30 -0600

    added missing import to Booklist

--- client/src/Components/BookList/BookList.js
+++ client/src/Components/BookList/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Thumbnail from "../Thumbnail";
+import { Container, Row, Col } from "../Grid";
 
 export function BookList({ children }) {
   return <ul className="list-group">{children}</ul>;
