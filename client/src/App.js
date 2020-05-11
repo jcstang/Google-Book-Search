34e74c188fbcf146181a07c06b0f68984f00331f
Brian Allen <brianallen2401@gmail.com>
2020-05-11 12:05:21 -0600

    updated app.js and added additional packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
-import Navbar from './Components/Navbar/Navbar'
-import Jumbotron from './Components/Jumbotron/Jumbotron'
-import SearchBar from './Components/SearchBar/SearchBar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved/Saved";
+import Search from "./pages/Search/Search";
+import Navbar from "./Components/Navbar/Navbar";
+import { Container } from "./Components/Grid/Grid";
+import { ToastContainer, Zoom } from "react-toastify";
+import Jumbotron from "./Components/Jumbotron/Jumbotron";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Router>
+          <Container>
+            <ToastContainer
+              position="top-center"
+              autoClose={3500}
+              transition={Zoom}
+              hideProgressBar
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnVisibilityChange
+              draggable={false}
+              pauseOnHover
+            />
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route path="/bookshelf" component={Saved} />
+              <Route exact path="/books/:id" component={null} />
+              <Route component={null} />
+            </Switch>
+          </Container>
+        </Router>
         <Jumbotron />
-        <SearchBar />
       </div>
     );
   }
