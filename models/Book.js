344bfe8150371789d5e8b167612d111b746d9b0a
Brian Allen <brianallen2401@gmail.com>
2020-05-11 12:20:13 -0600

    updated book model with required fields and renamed to match data on page

--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const BookSchema = new Schema({    
-    title: String,
-    authors: [String],
-    description: String,
-    image: String,
+const bookSchema = new Schema({    
+    title: {type: String, required: true },
+    authors: [{type: String, required: true }],
+    synopsis: String,
+    date: {type: Date, default: Date.now },
+    thumbnail: {type: String },
     link: String
 });
 
-const Book = mongoose.model('Book', BookSchema);
+const Book = mongoose.model('Book', bookSchema);
 module.exports = Book;
