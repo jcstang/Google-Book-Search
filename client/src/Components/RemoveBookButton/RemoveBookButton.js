328faac787750afd8c6170cea7d8919cbfea2b89
Brian Allen <brianallen2401@gmail.com>
2020-05-11 11:13:37 -0600

    added delete/remove book button component

---
+++ client/src/Components/RemoveBookButton/RemoveBookButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "../Button";
+
+function RemoveBookButton(props) {
+    return (
+        <Button type="danger" className="delete-btn" {...props} role="button" tabIndex="0">Delete</Button>
+    );
+}
