4a1fe1131cd4f7de1193a265383ae0b9544fb97f
Brian Allen <brianallen2401@gmail.com>
2020-05-11 13:03:12 -0600

    updated server layout and removed some un-necessary items

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
-
-const Book = require("./models/Book");
+const routes = require("./routes");
 
 // Send every request to the React app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 //Define API routes here
-app.get("/mongoose", function (request, response) {
-  Book.find({})
-  .then(function (data) {
-    response.json(data);
-  })
-});
+app.use(routes);
 
 // Define any API routes before this runs
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
